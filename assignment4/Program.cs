464d413ba634829ed50a0ef8644f27312391f84e
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:51:42 +0100

    Add files via upload

---
+++ assignment4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nrOfFullStops, nrOfCommas, nrOfSemiColons;
+
+
+            Console.WriteLine("Enter a text>");
+            string input = Console.ReadLine();
+            SearchText(input, out nrOfFullStops, out nrOfCommas, out nrOfSemiColons);
+
+            Console.Write(nrOfFullStops + " full stops, ");
+            Console.Write(nrOfCommas + " commas, ");
+            Console.WriteLine(nrOfSemiColons + " semicolons");
+        }   
+        static void SearchText(string text, out int nrOfFullStops, out int nrOfCommas, out int nrOfSemiColons)
+        {
+            nrOfFullStops = 0;
+            nrOfCommas = 0;
+            nrOfSemiColons = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '.')
+                {
+                    nrOfFullStops++;
+                }
+                else if (text[i] == ',')
+                {
+                    nrOfCommas++;
+                }
+                else if (text[i] == ';')
+                {
+                    nrOfSemiColons++;
+                }
+            }
+        }
+    }
+}
